bb5a9f136827a988e147dd4764d8a04861802ff1
maheshvinay1976-web <maheshvinay1976@gmail.com>
2026-03-16 21:44:37 +0530

    Add social media username checker function

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,10 +1,36 @@
-def validate_email(email):
-    return "@" in email and "." in email
+import requests
+
+def check_social(username):
+
+    sites = {
+        "Instagram": f"https://www.instagram.com/{username}",
+        "Twitter": f"https://twitter.com/{username}",
+        "GitHub": f"https://github.com/{username}",
+        "Reddit": f"https://www.reddit.com/user/{username}"
+    }
+
+    found = []
+
+    for site, url in sites.items():
+        try:
+            r = requests.get(url, timeout=5)
+
+            if r.status_code == 200:
+                found.append(site)
+
+        except:
+            pass
+
+    return found
+
 
 def categorize_risk(score):
+
     if score < 30:
         return "Low Risk"
+
     elif score < 70:
         return "Medium Risk"
+
     else:
         return "High Risk"
